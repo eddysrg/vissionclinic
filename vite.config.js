459ae65c02bf2fe6e6791e81c745efde825f9711
Eduardo Ramírez <eddysrg@outlook.com>
2024-09-23 15:21:01 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
             input: [
                 "resources/css/app.css",
                 "resources/js/app.js",
+                "resources/js/ece.js",
                 "resources/js/components/navbarMenu.js",
                 "resources/js/components/responsiveMenu.js",
             ],
